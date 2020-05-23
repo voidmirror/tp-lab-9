edab7aab29c6fab6101efed3fab018e387b44acd
Олеся Мартынюк <olesyamartinyuk@yandex.ru>
2020-05-23 19:02:07 +0300

    task2

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,15 @@
 
 #include <iostream>
 #include "task1.h"
+#include "task2.h"
 
 using namespace std;
-
 
 int main(int argc, const char * argv[]) {
     
     // task 1
+    
+    cout << " ============= TASK 1 ============= " << endl << endl;
     
     vector<string> array = {"i", "love", "quarantine", "so", "much", "somebody", "help", "me"};
     vector<string> res = sortBySizeUp(array);
@@ -31,5 +33,23 @@ int main(int argc, const char * argv[]) {
     
     res = sortByStringDown(array);
     printVector(res);
+    
+    
+    // task 2
+    
+    cout << endl << " ============= TASK 2 ============= " << endl << endl;
+    
+    res = array;
+    flagDone = false;
+    thread th(bubbleSortThreaded, std::ref(res), [](string a, string b){ return a > b ? true : false;});
+    while(!flagDone){
+        if (flagPrint){
+            mtx.lock();
+            printVector(res);
+            flagPrint = false;
+            mtx.unlock();
+        }
+    }
+    th.join();
     
 }
