d1b61d9c0898be18fb742fda35ad5ecd17ca5dc2
Olga Orlova <olia_orlova2000@mail.ru>
2020-05-26 14:58:34 +0300

    initial commit

---
+++ src/task1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+template <typename RandIterator, typename Comparator>
+void bubble_sort(
+	RandIterator first, RandIterator last,
+	Comparator comparator
+) {
+
+	for (RandIterator right = last-1; right > first; --right) {
+		for (RandIterator left = first; left < right; ++left) {
+			if (!comparator(*left, *right)) {
+				std::swap(*left, *right);
+			}
+		}
+	}
+}
+
+std::ostream& operator<< (std::ostream& out, const std::vector<std::string>& vec) {
+	for (auto x : vec) {
+		out << x << " ";
+	}
+	out << '\n';
+	return out;
+}
+
+int main() {
+	// #1
+	std::vector<std::string> vector1 = { "hello", "everyone", "who", "watch", "this", "code" };
+	bubble_sort(vector1.begin(), vector1.end(), [](const std::string & a, const std::string& b) {return a < b; });
+	std::cout << vector1;
+
+	// #2
+	std::vector<std::string> vector2 = { "deadlines", "research", "HSE", "code" };
+	bubble_sort(vector2.begin(), vector2.end(), [](const std::string & a, const std::string & b) {return a[0] > b[0]; });
+	std::cout << vector2;
+
+	// #3 
+	std::vector<std::string> vector3 = { "laba 9", "github", "repository" };
+	bubble_sort(vector3.begin(), vector3.end(), [](const std::string& a, const std::string& b) {return a + b < b + a; });
+	std::cout << vector3;
+
+	// #4
+	std::vector<std::string> vector4 = { "PMI-1", "PMI-2", "PI-1", "PI-2" };
+	bubble_sort(vector4.begin(), vector4.end(), [](const std::string& a, const std::string& b) {return a > b; });
+	std::cout << vector4;
+
+	// #5
+	std::vector<std::string> vector5 = { "deadlines", "research", "HSE", "code" };
+	bubble_sort(vector5.begin(), vector5.end(), []( std::string  a,  std::string  b) {
+		std::reverse(a.begin(), a.end());
+		std::reverse(b.begin(), b.end());
+		return  a < b; 
+			});
+	std::cout << vector5;
+
+	return 0;
+}
---
+++ src/task2.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+
+std::mutex mx;
+std::condition_variable cv;
+bool finished_sort = false;
+bool finished_iter = false;
+bool printed = false;
+
+template <typename RandIterator, typename Comparator>
+void bubble_sort(
+	RandIterator first, RandIterator last,
+	Comparator comparator
+) {
+	bool not_first_iter = false;
+	for (RandIterator right = last - 1; right > first; --right) {
+		for (RandIterator left = first; left < right; ++left) {
+			std::unique_lock<std::mutex> lk(mx);
+			if (not_first_iter)
+				cv.wait(lk, []() { return printed; });
+			if (!comparator(*left, *right)) {
+				std::swap(*left, *right);
+			}
+			finished_iter = true;
+			printed = false;
+			not_first_iter = true;
+			lk.unlock();
+			cv.notify_all();
+		}
+	}
+		finished_sort = true;
+}
+
+int main() {
+	std::vector<std::string> vec = { "hello", "everyone", "who", "watch", "this", "code" };
+
+	std::thread t(bubble_sort<std::vector<std::string>::iterator, bool(*)(const std::string &, const std::string&)>,
+		vec.begin(), vec.end(), [](const std::string& a, const std::string& b) {return a < b; });
+
+	while (!finished_sort) {
+		std::unique_lock<std::mutex> lk(mx);
+		cv.wait(lk, []() { return finished_iter; });
+		for (auto s : vec) {
+			std::cout << s << " ";
+		}
+		std::cout << std::endl;
+		finished_iter = false;
+		printed = true;
+		lk.unlock();
+		cv.notify_all();
+	}
+	
+	t.join();
+	
+	return 0;
+}
