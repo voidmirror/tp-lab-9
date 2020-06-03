39d339f40943f4faf1a50e8511ebfb64cb071d89
Olga Orlova <olia_orlova2000@mail.ru>
2020-06-03 12:57:06 +0300

    not_first_iter is removed

--- src/task2.cpp
+++ src/task2.cpp
@@ -8,36 +8,33 @@ std::mutex mx;
 std::condition_variable cv;
 bool finished_sort = false;
 bool finished_iter = false;
-bool printed = false;
+bool printed = true;
 
 template <typename RandIterator, typename Comparator>
 void bubble_sort(
 	RandIterator first, RandIterator last,
 	Comparator comparator
 ) {
-	bool not_first_iter = false;
 	for (RandIterator right = last - 1; right > first; --right) {
 		for (RandIterator left = first; left < right; ++left) {
 			std::unique_lock<std::mutex> lk(mx);
-			if (not_first_iter)
-				cv.wait(lk, []() { return printed; });
+			cv.wait(lk, []() { return printed; });
 			if (!comparator(*left, *right)) {
 				std::swap(*left, *right);
 			}
 			finished_iter = true;
 			printed = false;
-			not_first_iter = true;
 			lk.unlock();
 			cv.notify_all();
 		}
 	}
-		finished_sort = true;
+	finished_sort = true;
 }
 
 int main() {
 	std::vector<std::string> vec = { "hello", "everyone", "who", "watch", "this", "code" };
 
-	std::thread t(bubble_sort<std::vector<std::string>::iterator, bool(*)(const std::string &, const std::string&)>,
+	std::thread t(bubble_sort<std::vector<std::string>::iterator, bool(*)(const std::string&, const std::string&)>,
 		vec.begin(), vec.end(), [](const std::string& a, const std::string& b) {return a < b; });
 
 	while (!finished_sort) {
@@ -52,8 +49,8 @@ int main() {
 		lk.unlock();
 		cv.notify_all();
 	}
-	
+
 	t.join();
-	
+
 	return 0;
 }
