4f40b317079a0929cb7a6fea96fa970c1dbf437e
Polina Demochkina <lollipoli@live.com>
2020-06-06 23:55:35 +0300

    Finished all tasks

---
+++ src/main2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <mutex>
+#include <thread>
+#include <chrono>
+#include <ctime>
+
+std::mutex mtx;
+bool readyToPrint = true;
+bool strIsReady = false;
+
+template <typename T>
+void bubble_sort(std::vector<std::string>& strings, T lambda)
+{
+	for (int i = strings.size() - 1; i > 0; i--) {
+		mtx.lock();
+		for (int j = 0; j < i; j++) {
+			if (lambda(strings[j], strings[j + 1])) {
+				std::swap(strings[j], strings[j + 1]);
+			}
+		}
+		strIsReady = true;
+		mtx.unlock();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	}
+	readyToPrint = false;
+}
+
+int main()
+{
+	std::vector<std::string> vector = { "123", "321", "A" , "B", "C" , "a", "b", "z" };
+
+	std::thread secondThread(bubble_sort<bool(*)(const std::string&, const std::string&)>,
+		std::ref(vector), [](const std::string& a, const std::string& b) { return a < b; });
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	while (readyToPrint)
+	{
+		if (strIsReady)
+		{
+			mtx.lock();
+			for (int i = 0; i < vector.size(); i++)
+			{
+				std::cout << vector[i] << " ";
+			}
+			std::cout << std::endl;
+			strIsReady = false;
+			mtx.unlock();
+		}
+	}
+
+	std::cout << std::endl;
+	std::cout << "Run time: " << clock() / 1000.0;
+	secondThread.join();
+	return 0;
+}
---
+++ src/main3.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <ctime>
+
+bool NeedToOpenNew = true;
+int CountCashboxes = 0;
+
+void Signal(std::queue<std::vector<int>>& buyers)
+{
+	srand(time(NULL));
+	while (true)
+	{
+		if (buyers.size() == 5)
+		{
+			NeedToOpenNew = true;
+			return;
+		}
+		std::vector<int> purchases;
+		int NumberOfPurchases = rand() % 10 + 1;
+		for (int i = 0; i < NumberOfPurchases; i++)
+		{
+			purchases.push_back(rand() % 100 + 1);
+		}
+		buyers.push(purchases);
+
+		int TimeToNext = rand() % 3 + 1;
+		std::this_thread::sleep_for(std::chrono::seconds(TimeToNext));
+	}
+}
+
+void Cashboxes()
+{
+	
+	std::queue<std::vector<int>> buyers;
+
+	std::cout << "Cashboxe #" << std::this_thread::get_id() << " start work." << std::endl;
+
+	std::thread thread(Signal, std::ref(buyers));
+	thread.detach();
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	while (buyers.size() != 0)
+	{
+		std::cout << "Start new person: ";
+		for (int i = 0; i < buyers.front().size(); i++)
+		{
+			std::cout << buyers.front()[i] << " ";
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+		std::cout << std::endl;
+		buyers.pop();
+	}
+	std::cout << "Cashboxe #" << std::this_thread::get_id() << " stop work." << std::endl;
+	CountCashboxes--;
+}
+
+
+int main()
+{
+	do
+	{
+		if (NeedToOpenNew == true)
+		{
+			std::thread thread(Cashboxes);
+			NeedToOpenNew = false;
+			thread.detach();
+			CountCashboxes++;
+		}
+	} while (CountCashboxes > 0);
+}
