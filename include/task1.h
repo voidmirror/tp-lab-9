2ee1993cc3da7fe45bacdffc25a330ce83dc0078
smvolokh <sema.volokh@yandex.ru>
2020-06-07 20:23:28 +0300

    fix sort

--- include/task1.h
+++ include/task1.h
@@ -4,8 +4,8 @@
 
 
 std::vector <std::string> bubble(std::vector<std::string> arr, bool comp(std::string s1, std::string s2)) {
-	for (int i = 0; i < arr.size(); i++)
-		for (int j = 0; j < arr.size() - 1; j++)
+	for (int i = 0; i < arr.size()-1; i++)
+		for (int j = 0; j < arr.size() - i-1; j++)
 			if (comp(arr[j], arr[j+1])) {
 				auto temp = arr[i];
 				arr[i] = arr[j];
--- include/task2.h
+++ include/task2.h
@@ -9,8 +9,8 @@ bool iter = true;
 std::mutex mutex_1;
 
 void bubble_thread(std::vector<std::string>& arr, bool comp(std::string s1, std::string s2)) {
-	for (int i = 0; i < arr.size(); i++)
-		for (int j = 0; j < arr.size() - 1; j++) {
+	for (int i = 0; i < arr.size()-1; i++)
+		for (int j = 0; j < arr.size() - i-1; j++) {
 			if (comp(arr[j], arr[j+1])) {
 				mutex_1.lock();
 				std::swap(arr[j], arr[j+1]);
