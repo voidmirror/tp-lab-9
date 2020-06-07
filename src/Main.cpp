6334c2f2f778d194af18e233af3e40982cb0078d
Andrey Talanin <talanin.aa.nnov@gmail.com>
2020-06-07 14:22:44 +0300

    Do the second task

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,5 @@
-#define _TASK_1_MAIN_
+// #define _TASK_1_MAIN_
+#define _TASK_2_MAIN_
 
 #ifdef _TASK_1_MAIN_
 
@@ -10,10 +11,10 @@
 #include "Task1.h"
 
 template<typename T>
-void PrintArray(T *array, int32_t length)
+void PrintArray(T *array, int32_t arrayLength)
 {
-    for (int32_t i = 0; i < length; i++)
-        std::cout << array[i] << (i < length - 1 ? ", " : "");
+    for (int32_t i = 0; i < arrayLength; i++)
+        std::cout << array[i] << (i < arrayLength - 1 ? ", " : "");
     std::cout << std::endl;
 }
 
@@ -36,3 +37,83 @@ int32_t main()
 }
 
 #endif // _TASK_1_MAIN_
+
+#ifdef _TASK_2_MAIN_
+
+#include <algorithm>
+#include <condition_variable>
+#include <cstdint>
+#include <iostream>
+#include <mutex>
+#include <queue>
+#include <string>
+#include <thread>
+
+typedef std::unique_lock<std::mutex> unique_mutex_lock;
+
+std::mutex mutex;
+std::condition_variable conditionVariable;
+bool threadFinished = false;
+bool flag = false;
+
+std::string ArrayToString(std::string *array, int32_t arrayLength)
+{
+    std::string result;
+    for (int32_t i = 0; i < arrayLength; i++)
+        result += array[i] + (i < arrayLength - 1 ? ", " : "");
+    return result;
+}
+
+template<typename T>
+void BubbleSort(T *array, int32_t arrayLength, int32_t(*comparsion)(T *left, T *right))
+{
+    for (int32_t j = 0; j < arrayLength; j++)
+    {
+        bool anySwaps = false;
+
+        unique_mutex_lock lock(mutex);
+        conditionVariable.wait(lock, []() { return !flag; });
+        for (int32_t i = 0; i < arrayLength - j - 1; i++)
+        {
+            if (comparsion(array + i, array + i + 1) > 0)
+                std::swap(array[i], array[i + 1]), anySwaps = true;
+        }
+        flag = true;
+        lock.unlock();
+        conditionVariable.notify_all();
+
+        if (!anySwaps)
+        {
+            threadFinished = true;
+            return;
+        }
+    }
+};
+
+int32_t main()
+{
+    std::string stringArray[5] = { "CCC", "bbbb", "E", "dd", "AAAAA" };
+
+    std::cout << ArrayToString(stringArray, 5) << std::endl;
+
+    std::mutex mutex;
+    std::thread thread([](std::string *array, int32_t arrayLength, int32_t(*comparsion)(std::string *left, std::string *right)) { BubbleSort<std::string>(array, arrayLength, comparsion); },
+        stringArray, 5, [](std::string *left, std::string *right) { return (int32_t)left->size() - (int32_t)right->size(); });
+
+    conditionVariable.notify_all();
+    while (!threadFinished)
+    {
+        unique_mutex_lock lock(mutex);
+        conditionVariable.wait(lock, []() { return flag; });
+
+        std::cout << ArrayToString(stringArray, 5) << std::endl;
+
+        flag = false;
+         lock.unlock();
+        conditionVariable.notify_all();
+    }
+
+    thread.join();
+}
+
+#endif // _TASK_2_MAIN_
