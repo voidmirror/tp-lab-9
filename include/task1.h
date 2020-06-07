ddca073c3395f4a138a53b0c964305359e41b2f3
smvolokh <55282457+smvolokh@users.noreply.github.com>
2020-06-07 20:35:21 +0300

    Update task1.h

--- include/task1.h
+++ include/task1.h
@@ -7,9 +7,9 @@ std::vector <std::string> bubble(std::vector<std::string> arr, bool comp(std::st
 	for (int i = 0; i < arr.size()-1; i++)
 		for (int j = 0; j < arr.size() - i-1; j++)
 			if (comp(arr[j], arr[j+1])) {
-				auto temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+				auto temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
 			}
 	return arr;
 }
