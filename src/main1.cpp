582863357e68baab3af12d614893fd6a87d0b22d
Roman Ershov <50735876+PharrosTheVagabond@users.noreply.github.com>
2020-06-09 02:24:15 +0300

    Changed placeholders for different comparators

--- src/main1.cpp
+++ src/main1.cpp
@@ -6,11 +6,11 @@ int main() {
 		[](const string& s1, const string& s2) -> int { return s1.compare(s2); },
 		[](const string& s1, const string& s2) -> int { return s2.compare(s1); },
 		[](const string& s1, const string& s2) -> int { return s1.size() - s2.size(); },
-		[](const string& s1, const string& s2) -> int { return s1.size() - s2.size(); },
-		[](const string& s1, const string& s2) -> int { return s1.size() - s2.size(); }
+		[](const string& s1, const string& s2) -> int { return s2.size() - s1.size(); },
+		[](const string& s1, const string& s2) -> int { return s1.back() - s2.back(); }
 	};
 
-	vector<string> arr = { "33333", "2222", "111", "00", "5" };
+	vector<string> arr = { "cdefg", "aba", "dz", "bbbb", "e" };
 	cout << "Before sort: " << endl;
 	for (auto i = arr.begin(); i < arr.end(); i++) {
 		cout << *i << " ";
